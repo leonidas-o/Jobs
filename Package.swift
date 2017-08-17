8a658ac8299d70944b91e1316405f4cba09b964a
Leonid Orsulic <lorsulic@Leonids-MBP16.fritz.box>
2017-08-17 18:54:25 +0200

    adds products .library

--- Package.swift
+++ Package.swift
@@ -3,6 +3,12 @@ import PackageDescription
 
 let package = Package(
     name: "Jobs",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Jobs",
+            targets: ["Jobs"]),
+    ],
     targets: [
         .target(
             name: "Jobs",
